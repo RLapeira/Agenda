bdfbcca688ccea214e29990d69d3c111e03488aa
Rocío Lapeira Naranjo <r.lapeira.naranjo@avanade.com>
2024-04-10 12:12:21 +0200

    Añadir atributos a la clase Contacto

--- Contacto.cs
+++ Contacto.cs
@@ -8,5 +8,16 @@ namespace Agenda
 {
     public class Contacto
     {
+        private int id;
+        private string name;
+        private DateTime fechaNacimiento;
+        private string telefono;
+        private string observaciones;
+
+        public int Id { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Telefono { get; set; }
+        public string Observaciones { get; set; }
+
     }
 }
