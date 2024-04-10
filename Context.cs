b90643116494c5458dc1a0f1348db5d86574c13a
Rocío Lapeira Naranjo <r.lapeira.naranjo@avanade.com>
2024-04-10 12:41:35 +0200

    Añadir metodos Connect y Disconnect a la clase Context

--- Context.cs
+++ Context.cs
@@ -1,12 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace Agenda
 {
     public class Context
     {
+        // Nombre del servidor que contiene la base de datos
+        static string DataSource = "WINAPY7QPNFXDES\\SQLEXPRESS04";
+
+        // Nombre de la base de datos
+        static string InitialCatalog = "Agenda";
+
+        // Establece si la conexión debe usar seguridad integrada.
+        // Si se establece en "SSPI", se utilizará la autenticación de Windows del usuario
+        static string IntegratedSecurity = "SSPI";
+
+        // Cadena de conexión
+        static string connectionString = $"Data Source={DataSource}; " +
+            $"Initial Catalog={InitialCatalog}; " +
+            $"Integrated Security={IntegratedSecurity}";
+
+        SqlConnection connection = new SqlConnection(connectionString);
+
+        public void Connect()
+        {
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Conexión abierta");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("¡Error al conectar a la base de datos!\n" + ex.Message);
+            }
+        }
+
+        public void Disconnect()
+        {
+            try
+            {
+                connection.Close();
+                connection.Dispose();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("¡Error al cerrar la conexión con base de datos!\n" + ex.Message);
+            }
+        }
+
     }
 }
