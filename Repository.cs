21e8a5d73b1237530e31f51b9649a4cedc38f516
Rocío Lapeira Naranjo <r.lapeira.naranjo@avanade.com>
2024-04-10 13:38:03 +0200

    Añadir método getAllContactos en el repositorio

--- Repository.cs
+++ Repository.cs
@@ -1,12 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace Agenda
 {
     public class Repository
     {
+        SqlConnection con = new Context().Connect();
+
+        public List<Contacto> getAllContactos()
+        {
+            List<Contacto> contactos = new List<Contacto>();
+
+            string sql = "Select Id, " +
+                "Nombre, " +
+                "FechaNacimiento, " +
+                "Telefono, " +
+                "Observaciones " +
+                "from dbo.Contactos";
+            SqlCommand command = new SqlCommand(sql, con);
+            
+            SqlDataReader dataReader = command.ExecuteReader();
+
+            Contacto contacto;
+
+            while(dataReader.Read())
+            {
+                contacto = new Contacto((int)dataReader.GetValue(0),
+                    (string)dataReader.GetValue(1),
+                    (DateTime)dataReader.GetValue(2),
+                    (string)dataReader.GetValue(3),
+                    (string)dataReader.GetValue(4));
+
+                contactos.Add(contacto);
+            }
+            return contactos;
+        }
     }
 }
